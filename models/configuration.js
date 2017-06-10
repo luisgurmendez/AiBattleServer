634d4cf6ec0a94e25bdec3fc2e3ffd1c52e323c9
Nicolas Ferro <nferro@correo.um.edu.uy>
2017-06-10 12:26:25 -0300

    Undo previous commit

--- models/configuration.js
+++
@@ -1,7 +0,0 @@
-var exports = module.exports = {};
-
-var database_host = 'localhost';
-var database_port = 27017;
-var database_name = 'test';
-exports.database_url = "mongodb://" + database_host + ":" + database_port + "/" + database_name;
-
